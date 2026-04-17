5d3e5da2f39f3f56cda6dc81c03bb509381d6d71
Christian Haddrell <christian@haddrell.co.uk>
2026-04-17 07:04:36 +0100

    Set real site metadata and URL
    Remove Atkinson font wiring (Font component + CSS var) so the build is
    clean ahead of Task 2 which installs IBM Plex fonts properly.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Christian Haddrell';
+export const SITE_SHORT = 'Haddrell';
+export const SITE_DESCRIPTION = 'Product engineering, with receipts.';
+export const SITE_URL = 'https://www.haddrell.co.uk';
+export const AUTHOR_NAME = 'Christian Haddrell';
+export const AUTHOR_EMAIL = '***REMOVED-EMAIL***';
+export const PSC_NAME = 'Dougal Debug Limited';
